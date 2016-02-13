c020c21f5387ebb0c16ca0b59e64e69abe4501ae
Sean <sean.t.irby@gmail.com>
2016-02-12 17:28:11 -0800

    Formatting

--- evalator.el
+++ evalator.el
@@ -1,5 +1,5 @@
 ;;; evalator.el --- Package for interactive transformation of data with helm
-;; 
+;;
 ;; Copyright © , Sean Irby
 ;; Author: Sean Irby
 ;; Maintainer: Sean Irby <sean.t.irby@gmail.com>
@@ -7,26 +7,26 @@
 ;; Version: 1.0.0
 ;; Keywords: languages, elisp, helm
 ;; Package-Requires: ((helm-core "1.9.1"))
-;; 
+;;
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or (at
 ;; your option) any later version.
-;; 
+;;
 ;; This program is distributed in the hope that it will be useful, but
 ;; WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;; General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
-;; 
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; This file is not a part of GNU Emacs
-;; 
+;;
 ;;; Commentary:
-;; 
+;;
 ;;; Code:
 
 (require 'cl-lib)
